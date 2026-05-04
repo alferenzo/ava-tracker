cfc8adfbbdd7650c4338773c9457d0422911136e
alferenzo <alferenzot@gmail.com>
2026-05-04 22:59:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
-// The Service Worker for Push Notifications & Offline Caching
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
+const CACHE_NAME = "ava-care-cache-v1";
+const urlsToCache = [
+  "./Index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', (e) => {
-    e.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (e) => {
-    // Allows the app to work offline once installed
+// THIS FETCH LISTENER IS MANDATORY FOR ANDROID TO TREAT IT AS A REAL APP
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Return from cache
+        }
+        return fetch(event.request); // Or fetch from network
+      })
+  );
 });
